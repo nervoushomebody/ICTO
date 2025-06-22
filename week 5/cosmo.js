7d1ea7bc46d9ea11ceeeac6d6d436c1cbeb71862
nervoushomebody <132596641+nervoushomebody@users.noreply.github.com>
2025-06-22 23:50:29 +0300

    Update cosmo.js

--- week 5/cosmo.js
+++ week 5/cosmo.js
@@ -52,3 +52,46 @@ AFRAME.registerComponent('main', {
  		}
  	}
 });
+tick: function(time, timeDelta) {
+  // Крок інтегрування (1/3 земного дня)
+  const dt = 86400 / 3;
+  
+  // Розрахунок прискорення для всіх планет
+  for (let i = 0; i < planets.length; i++) {
+    planets[i].a = [0, 0, 0];
+    
+    for (let j = 0; j < planets.length; j++) {
+      if (i !== j) {
+        // Вектор відстані між планетами
+        let r = [
+          planets[j].pos[0] - planets[i].pos[0],
+          planets[j].pos[1] - planets[i].pos[1],
+          planets[j].pos[2] - planets[i].pos[2]
+        ];
+        
+        // Модуль відстані
+        let dist = Math.sqrt(r[0]*r[0] + r[1]*r[1] + r[2]*r[2]);
+        
+        // Розрахунок прискорення за законом Ньютона
+        for (let k = 0; k < 3; k++) {
+          planets[i].a[k] += G * planets[j].mass * r[k] / Math.pow(dist, 3);
+        }
+      }
+    }
+  }
+  
+  // Оновлення швидкості та координат
+  for (let i = 0; i < planets.length; i++) {
+    for (let k = 0; k < 3; k++) {
+      planets[i].v[k] += planets[i].a[k] * dt;
+      planets[i].pos[k] += planets[i].v[k] * dt;
+    }
+    
+    // Оновлення позиції 3D-об'єкта
+    planets[i].el.setAttribute('position', {
+      x: planets[i].pos[0] / 1000000000,
+      y: planets[i].pos[1] / 1000000000,
+      z: planets[i].pos[2] / 1000000000
+    });
+  }
+}
